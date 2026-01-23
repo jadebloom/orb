ce924664603b76a66e6d2213f28c2f51c3186712
jadebloom <warfacemeister@mail.ru>
2026-01-22 23:27:09 -0500

    chore: added tailwindcss-primeui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,9 @@ export const appConfig: ApplicationConfig = {
 		providePrimeNG({
 			theme: {
 				preset: Aura,
+				options: {
+					darkModeSelector: '.ch-dark-mode',
+				},
 			},
 		}),
 	],
