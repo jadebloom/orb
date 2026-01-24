e6929368340a9db9235f348dccf5b230d0b537c8
jadebloom <warfacemeister@mail.ru>
2026-01-24 01:10:22 -0500

    chore: added storybook theme toggle addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-onboarding'],
+	addons: [
+		'@storybook/addon-a11y',
+		'@storybook/addon-docs',
+		'@storybook/addon-onboarding',
+		'@storybook/addon-themes',
+	],
 	framework: '@storybook/angular',
 	core: {
 		disableTelemetry: true, // 👈 Disables telemetry
