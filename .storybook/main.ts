4bb4690bfa94580f456cdedbc16b27bd2e689993
jadebloom <warfacemeister@mail.ru>
2026-01-24 00:56:23 -0500

    chore: added storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,11 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-onboarding'],
+	framework: '@storybook/angular',
+	core: {
+		disableTelemetry: true, // 👈 Disables telemetry
+	},
+};
+export default config;
