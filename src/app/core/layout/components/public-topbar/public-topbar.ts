1b292ff173352f3efd7cede88564572e09c7ec6a
jadebloom <warfacemeister@mail.ru>
2026-01-26 00:35:02 -0500

    feat: added github link in public topbar

--- src/app/core/layout/components/public-topbar/public-topbar.ts
+++ src/app/core/layout/components/public-topbar/public-topbar.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { DarkModeToggle } from '@core/theme/components/dark-mode-toggle/dark-mode-toggle';
 import { ColorSchemeConfigurator } from '@core/theme/components/color-scheme-configurator/color-scheme-configurator';
 
 @Component({
 	selector: 'orb-public-topbar',
-	imports: [DarkModeToggle, ColorSchemeConfigurator],
+	imports: [ButtonModule, DarkModeToggle, ColorSchemeConfigurator],
 	templateUrl: './public-topbar.html',
 })
-export class PublicTopbar {}
+export class PublicTopbar {
+	visitGithub() {
+		window.open('https://github.com/the-jade-chamber/orb', '_blank', 'noopener,noreferrer');
+	}
+}
